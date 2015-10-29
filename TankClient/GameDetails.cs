29725ae2176f3e8f6ccd357a1618590068822d14
chanakadinesh <cdkbalasooriya.13@cse.mrt.ac.lk>
2015-10-29 07:11:55 +0530

    Coins and Lifepacks paser implemented

--- TankClient/GameDetails.cs
+++ TankClient/GameDetails.cs
@@ -9,25 +9,35 @@ namespace TankClient
     class GameDetails
     {
         private String myId;
-        private String[,] playerData; //Name,Score,Coins,Health
+        private String[,] playerData; //Name,Health,Coins,Score
         private int[,] playerLocations; //x,y,direct
+        private int p_count;  //number of players;
         private int[,] bricks; //x,y,damageLevel
         private int[] count; //number of bricks,water,stone
         private int[,] water;
         private int[,] stone;
         private bool gameStarted;
         private bool gameConnected;
+        private bool hasplayerDetails;
+
+        private List<coin> coinList;
+        private List<lifepack> lifepackList;
+
         Parser paser;
         public GameDetails()
         {
             paser = new Parser(this);
             gameConnected = false;
             gameStarted = false;
+            hasplayerDetails = false;
             myId = "P#";
             playerData = new String[5, 4];
             playerLocations = new int[5, 3];
-
+            p_count = 0;
             count = new int[3];
+            coinList =new List<coin>{};
+            lifepackList = new List<lifepack> { };
+
         }
         public bool isGameStarted
         {
@@ -38,6 +48,39 @@ namespace TankClient
             set
             {
                 gameStarted = value;
+            }
+        }
+        public List<coin> Coins
+        {
+            get
+            {
+                return coinList;
+            }
+            set
+            {
+                coinList = value;
+            }
+        }
+        public List<lifepack> LifePacks
+        {
+            get
+            {
+                return lifepackList;
+            }
+            set
+            {
+                lifepackList = value;
+            }
+        }
+        public bool hasPlayerDetails
+        {
+            get
+            {
+                return hasplayerDetails;
+            }
+            set
+            {
+                hasplayerDetails = value;
             }
         }
 
@@ -98,5 +141,77 @@ namespace TankClient
         public void update(String s) {
             paser.ParseMessage(s);
         }
+        
+        public int[,] playerLoc{
+            get { return playerLocations; }
+            set { playerLocations = value; }
+        }
+        public string[,] playerDetail
+        {
+            get { return playerData; }
+            set { playerData = value; }
+        }
+        public int playerCount
+        {
+            get { return p_count; }
+            set { p_count = value; }
+        }
+    }
+
+    class coin
+    {
+        private int x, y, l, v;
+        public coin(int x, int y, int l, int v)
+        {
+            this.x = x;
+            this.y = y;
+            this.l = l;
+            this.v = v;
+        }
+        public int X{
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int L
+        {
+            get { return l; }
+            set { l = value; }
+        }
+        public int V
+        {
+            get { return v; }
+            set { v = value; }
+        }
+    }
+
+    class lifepack
+    {
+        private int x,y,l;
+        public lifepack(int x, int y, int l)
+        {
+            this.x=x;
+            this.y=y;
+            this.l=l;
+        }
+        public int X{
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int L
+        {
+            get { return l; }
+            set { l = value; }
+        }
+
     }
 }
--- TankClient/Parser.cs
+++ TankClient/Parser.cs
@@ -18,7 +18,8 @@ namespace TankClient
             {
                 case "S:":
                     //.myPlayerNumber = (int)Char.GetNumericValue(s[3]);
-                    gd.myPlayerNumber = s.Substring(2, 2);
+                  //  gd.myPlayerNumber = s.Substring(2, 2);
+                    parseStart(s);  
                     gd.isGameStarted = true;
                     break;
                 case "I:":
@@ -26,15 +27,40 @@ namespace TankClient
                     gd.Connected = true;
                     break;
                 case "G:":
+                    parseGround(s);
+                    gd.hasPlayerDetails = true;
                     break;
                 case "L:":
+                    parseLifepacks(s);
                     break;
                 case "C:":
+                    parseCoins(s);
                     break;
                 default:
                     break;
             }
         }
+
+        private void parseStart(string s)
+        {
+            s = s.Substring(0, s.Length - 2);
+            string[] parts = s.Split(':');
+            gd.playerCount = parts.Length - 1;
+            int[,] loc = new int[parts.Length - 1, 3];
+           // string[,] p_data = new string[parts.Length - 1, 4];
+            for (int i = 1; i < parts.Length; i++)
+            {
+                string y = parts[i];
+                string []y1 = y.Split(';');
+                string[] y2 = y1[1].Split(',');
+                loc[i - 1, 0] = Convert.ToInt32(y2[0]);
+                loc[i - 1, 1] = Convert.ToInt32(y2[1]);
+                loc[i - 1, 2] = Convert.ToInt32(y1[2]);
+
+            }
+            gd.playerLoc = loc;
+        }
+
         private void parseInit(String s)
         {
             string[] parts = s.Split(':');
@@ -48,11 +74,7 @@ namespace TankClient
                 string [] locNum =loc.Split(',');
                 bricksLoc[count,0] = Convert.ToInt32(locNum[0]);
                 bricksLoc[count, 1] = Convert.ToInt32(locNum[1]);
-                if (count % 4 == 0) { bricksLoc[count, 2] = 0; }
-                else if (count % 4 == 1) { bricksLoc[count, 2] = 1; }
-                else if (count % 4 == 2) { bricksLoc[count, 2] = 2; }
-                else if (count % 4 == 3) { bricksLoc[count, 2] = 3; }
-                else { }
+                bricksLoc[count, 2] = 0;
                 count++;
             }
             gd.Bricks = bricksLoc;
@@ -86,6 +108,70 @@ namespace TankClient
                 count++;
             }
             gd.Stone = stoneLoc;
+        }
+
+        private void parseGround(String s) {
+            s = s.Substring(0, s.Length - 2);
+            string[] parts = s.Split(':');
+            string[,] p_data = gd.playerDetail;
+            int[,] loc = gd.playerLoc;
+            int[,] brick = gd.Bricks;
+            foreach (string y in parts)
+            {
+                if (y == "G") { }
+                else if (y[0] == 'P')
+                {
+
+                    int p_id = (int)Char.GetNumericValue(y[1]);
+                    string [] y1=y.Split(';');
+                    p_data[p_id, 0] = y1[0];
+                    string[] y2 = y1[1].Split(',');
+                    loc[p_id, 0] = Convert.ToInt32(y2[0]);
+                    loc[p_id, 1] = Convert.ToInt32(y2[1]);
+                    loc[p_id, 2] = Convert.ToInt32(y1[2]);
+                    p_data[p_id, 1] = y1[4];
+                    p_data[p_id, 2] = y1[5];
+                    p_data[p_id, 3] = y1[6];
+                }
+                else {
+                    string[] y1 = y.Split(';');
+                    for(int i=0;i<y1.Length;i++){
+                        string[] y2 = y1[i].Split(',');
+                        brick[i, 0] = Convert.ToInt32(y2[0]);
+                        brick[i, 1] = Convert.ToInt32(y2[1]);
+                        brick[i, 2] = Convert.ToInt32(y2[2]);
+                    }
+                    
+                }
+            }
+            gd.playerDetail = p_data;
+            gd.playerLoc = loc;
+            gd.Bricks = brick;
+        }
+
+        private void parseCoins(String s) {
+            s = s.Substring(0, s.Length - 2);
+            string[] parts = s.Split(':');
+            List<coin> co = gd.Coins;
+            string[] y = parts[1].Split(',');
+            int lx = Convert.ToInt32(y[0]);
+            int ly = Convert.ToInt32(y[1]);
+            int lt = Convert.ToInt32(parts[2]);
+            int lv = Convert.ToInt32(parts[3]);
+            co.Add(new coin(lx, ly, lt, lv));
+            gd.Coins = co;
+        }
+        private void parseLifepacks(string s) {
+            s = s.Substring(0, s.Length - 2);
+            string[] parts = s.Split(':');
+            List<lifepack> lp = gd.LifePacks;
+            string[] y = parts[1].Split(',');
+            int lx = Convert.ToInt32(y[0]);
+            int ly = Convert.ToInt32(y[1]);
+            int lt = Convert.ToInt32(parts[2]);
+            //int lv = Convert.ToInt32(parts[3]);
+            lp.Add(new lifepack(lx, ly, lt));
+            gd.LifePacks = lp;
         }
     }
 }
